ae0917d9c0bf9d4a07f209543c7b499a8948cfa9
Ben Nguyen <benc.nguyen02@gmail.com>
2024-10-03 12:01:08 -0700

    owowowowow

--- src/code/ca/bcit/comp2522/lab5/BAM/Main.java
+++ src/code/ca/bcit/comp2522/lab5/BAM/Main.java
@@ -1,7 +1,7 @@
 package ca.bcit.comp2522.lab5.BAM;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         System.out.println("Hello world!");
     }
 }
