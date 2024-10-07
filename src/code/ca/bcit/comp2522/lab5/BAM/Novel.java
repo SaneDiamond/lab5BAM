45b6b3368c3afe01e0b4a2c835aa7f5efbdcbf65
MarcusLages <marcusvlages@gmail.com>
2024-10-06 21:18:23 -0700

    Added instance variables, getters and compareTo implementation for Novel.java

--- src/code/ca/bcit/comp2522/lab5/BAM/Novel.java
+++ src/code/ca/bcit/comp2522/lab5/BAM/Novel.java
@@ -1,4 +1,34 @@
 package ca.bcit.comp2522.lab5.BAM;
 
-public class Novel {
+public class Novel
+        implements Comparable<Novel> {
+    private final String title;
+    private final String authorName;
+    private final int yearPublished;
+
+    public Novel(final String title, final String authorName, final int yearPublished) {
+        this.title = title;
+        this.authorName = authorName;
+        this.yearPublished = yearPublished;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public int getYearPublished() {
+        return yearPublished;
+    }
+
+    @Override
+    public int compareTo(final Novel novel) {
+        final int comparison;
+        comparison = title.compareTo(novel.title);
+
+        return comparison;
+    }
 }
